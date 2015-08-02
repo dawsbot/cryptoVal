db3482a82791c724e56d1349cdfde86e274db12b
dawsonbotsford <3408480+dawsbot@users.noreply.github.com>
2015-08-01 22:32:15 -0700

    Further refactor. Things are looking clean!

--- aserver.js
+++ aserver.js
@@ -1,15 +1,6 @@
 var mongoose = require('mongoose');
-var uriUtil = require('mongodb-uri');
 var async = require('async');
 
-var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } },
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } };
-
-var mongodbUri = process.env.MONGOLAB_URI;
-var mongooseUri = uriUtil.formatMongoose(mongodbUri);
-mongoose.connect(mongooseUri, options);
-
-var conn = mongoose.connection;
 
 conn.on('error', console.error.bind(console, 'connection error:'));
 
---
+++ routes/message.js
@@ -0,0 +1,38 @@
+var twilio = require('twilio');
+var converter = require('satoshi-bitcoin');
+var config = require('../config');
+var Chain = require('chain-node');
+var validator = require('bitcoin-address');
+
+module.exports = function(request, response){
+  var phone = request.body.From;
+  var input = request.body.Body;
+
+  var chain = new Chain({
+    keyId: config.chainApiKeyId,
+    keySecret: config.chainApiKeySecret,
+    blockChain: 'bitcoin'
+  });
+
+  var respond = function(message) {
+    var twiml = new twilio.TwimlResponse();
+    twiml.message(message);
+    response.writeHead(200, {'Content-Type': 'text/xml'});
+    response.end(twiml.toString());
+  };
+
+  //see if its a valid btc address
+  if (validator.validate(input) === true) {
+    chain.getAddress(input, function(err, blockchainResponse) {
+      if (err){
+        console.log('ERROR calling blockchain API with input: ' + input);
+        console.log('The API blockchainResponse was: ' + blockchainResponse);
+        return;
+      }
+      respond('That wallet contains ' + converter.toBitcoin(blockchainResponse[0].total.balance) + ' BTC');
+    });
+  }
+  else {
+    respond(input + ' is not a valid BTC address. Please try again');
+  }
+}
