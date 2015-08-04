4dd6a05f57c23faa7f8d9e7d5022054d4d276ff3
dawsonbotsford <3408480+dawsbot@users.noreply.github.com>
2015-08-03 23:03:35 -0700

    Support Dogecoin and Litecoin! Closes #6, closes #5

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  chainApiKeyId: process.env.CHAIN_API_KEY_ID,
-  chainApiKeySecret: process.env.CHAIN_API_KEY_SECRET,
   mongoUrl: process.env.MONGOLAB_URI,
 
   //MongoLab specific options
