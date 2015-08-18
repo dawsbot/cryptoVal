71ce5cd439f7301095dd6189c1b08e7e66c0a9c6
ColeHud <cmhudson11@gmail.com>
2015-08-18 13:34:51 -0400

    Fixed issue #13
    convert double to int if it has no relevant decimal places

--- routes/message.js
+++ routes/message.js
@@ -18,13 +18,26 @@ module.exports = function(request, response){
       response.end(twiml.toString());
     };
 
+    //convert a double to int if it doesn't have relevant decimal places
+    var convertNumber = function(number){
+      var doubleNum = number;
+      var intNum = (doubleNum | 0);
+
+      if(doubleNum > intNum || doubleNum < intNum){
+        return doubleNum;
+      }
+      else{
+        return intNum;
+      }
+    };
+
     //see if its a valid crypto address
     sochain.getCryptoType(parsedMessage, function(sochainResponse) {
       if (sochainResponse === 'error' || sochainResponse === 'none') {
         respond('\"' + parsedMessage + '\" is not a valid crypto address!\nKeep in mind we support BTC, LTC, and Doge');
       }
       else {
-          respond('That\'s a ' + sochainResponse.data.network + ' wallet with a balance of ' + sochainResponse.data.balance + ' and ' + sochainResponse.data.pending_value + ' ' + sochainResponse.data.network + '\'s pending.');
+          respond('That\'s a ' + sochainResponse.data.network + ' wallet with a balance of ' + convertNumber(sochainResponse.data.balance) + ' and ' + convertNumber(sochainResponse.data.pending_value) + ' ' + sochainResponse.data.network + '\'s pending.');
       }
     });
   });
